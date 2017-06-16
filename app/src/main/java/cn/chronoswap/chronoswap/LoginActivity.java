8c85ec5daf0b788696b73dcbd45f462da6beae07
WH_hunter <814188639@qq.com>
2017-06-16 18:23:25 +0800

    Login(90%FINISH)

--- app/src/main/java/cn/chronoswap/chronoswap/LoginActivity.java
+++ app/src/main/java/cn/chronoswap/chronoswap/LoginActivity.java
@@ -1,12 +1,19 @@
 package cn.chronoswap.chronoswap;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -18,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
     private TextView tvError;
     private Button btnLogin;
     private String test;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,52 +36,66 @@ public class LoginActivity extends AppCompatActivity {
         tvError = (TextView) findViewById(R.id.login_tvError);
         btnLogin = (Button) findViewById(R.id.login_btnLogin);
         btnLogin.setOnClickListener(listener);
+        mHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == 2) {
+                    tvError.setTextColor(getResources().getColor(R.color.right));
+                    tvError.setText((String) msg.obj);
+                    finish();
+                } else tvError.setText((String) msg.obj);
+            }
+        };
     }
 
     private View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            test = Login();
-            if(test == null){
-                tvError.setText("请输入用户名或密码");
-            }
-            else if (test.charAt(0) == 'F') {
-                tvError.setText("用户名或密码错误");
-            } else if (test.charAt(0) == 'T') {
-                tvError.setTextColor(getResources().getColor(R.color.right));
-                tvError.setText("登录成功");
-                finish();
-            }
+            Login();
         }
     };
+
     //登录操作
-    protected String Login() {
+    protected void Login() {
         String username = etUsername.getText().toString();
         String password = etPassword.getText().toString();
-        String path = "http://www.chronoswap.cn/userlogin.php";
+        String path = "http://www.chronoswap.cn/user_login.php";
         //创建okHttpClient对象
         OkHttpClient ohc = new OkHttpClient();
         //表单数据
         RequestBody fb = new FormBody.Builder()
-                .addEncoded("name", username)
-                .addEncoded("password", password)
+                .add("name", username)
+                .add("password", password)
                 .build();
         //创建一个Request
         Request req = new Request.Builder()
                 .url(path)
                 .post(fb)
                 .build();
-        Response res;
-        try {
-            res = ohc.newCall(req).execute();
-            String a = res.body().string();
-            return a;
-        } catch (Exception e) {
-            return null;
-        }
+        Call call = ohc.newCall(req);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
 
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String test = response.body().string();
+                Message msg = new Message();
+                if (test == null) {
+                    msg.what = 0;
+                    msg.obj = "请输入用户名或密码";
+                } else if (test.charAt(0) == 'F') {
+                    msg.what = 1;
+                    msg.obj = "用户名或密码错误";
+                } else if (test.charAt(0) == 'T') {
+                    msg.what = 2;
+                    msg.obj = "登录成功";
+                }
+                mHandler.sendMessage(msg);
+            }
+        });
     }
-
 }
 /*
     public void Login(View v) {
