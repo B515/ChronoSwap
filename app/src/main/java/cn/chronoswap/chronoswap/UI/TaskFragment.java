02b717ef00553f5cae389ab99d092fc71272af3d
ZeroGoYoosee <qz777zp@live.cn>
2017-09-03 22:48:26 +0800

    Toobarrrrrrrrrrr!!!!!!!!!!!!!!!!!!!!!
    Toobarrrrrrrrrrr!!!!!!!!!!!!!!!!!!!!!
    Toobarrrrrrrrrrr!!!!!!!!!!!!!!!!!!!!!
    Toobarrrrrrrrrrr!!!!!!!!!!!!!!!!!!!!!

--- app/src/main/java/cn/chronoswap/chronoswap/UI/TaskFragment.java
+++ app/src/main/java/cn/chronoswap/chronoswap/UI/TaskFragment.java
@@ -14,7 +14,7 @@ public class TaskFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_issued_list, container, false);
+        return inflater.inflate(R.layout.fragment_task_list, container, false);
     }
 
     @Override
