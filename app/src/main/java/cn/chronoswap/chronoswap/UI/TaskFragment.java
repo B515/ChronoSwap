f05f056eba150dccc6a37b2f5f66abec2c21059e
Personal_GH <bjshizimo@163.com>
2017-11-06 15:52:00 +0800

    ？？？

--- app/src/main/java/cn/chronoswap/chronoswap/UI/TaskFragment.java
+++ app/src/main/java/cn/chronoswap/chronoswap/UI/TaskFragment.java
@@ -2,6 +2,8 @@ package cn.chronoswap.chronoswap.UI;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,27 +13,40 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import cn.chronoswap.chronoswap.R;
+import cn.chronoswap.chronoswap.db.UserInfoManager;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class TaskFragment extends ListFragment {
 
     private String TAG = TaskFragment.class.getName();
-    private ListView list ;
+    private ListView list;
     private SimpleAdapter adapter;
+    private Handler mHandler;
+    private int num;
+    private String[][] strand;
 
     /**
      * @描述 在onCreateView中加载布局
-     * */
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_task_list, container,false);
+        //创建viwe和list对象
+        View view = inflater.inflate(R.layout.fragment_task_list, container, false);
         list = (ListView) view.findViewById(android.R.id.list);
         Log.i(TAG, "--------onCreateView");
         return view;
@@ -42,39 +57,99 @@ public class TaskFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         //b = savedInstanceState;
         Log.i(TAG, "--------onCreate");
-        String[] list = {"Class 1","Class 2","class 3","Class 4","Class 5"};
+        //loadlist();         //调用函数
+        String[][] list = strand;
+        //设置adapter
         adapter = new SimpleAdapter(getActivity(), getData(list), R.layout.fragment_task, new String[]{"payment", "time", "title"}, new int[]{R.id.task_tv_payment, R.id.task_tv_task, R.id.task_tv_time});
         setListAdapter(adapter);
 
     }
 
+    //loadlist
+    protected void loadlist() {
+
+        //TelephonyManager TelephonyMgr = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        //String imei = TelephonyMgr.getDeviceId();
+        String imei = "355905073732092";     //需要修改355905073732092
+        String path = "http://www.chronoswap.cn/mission_info.php";
+        //创建okHttpClient对象
+        OkHttpClient ohc = new OkHttpClient();
+        //表单数据
+        RequestBody fb = new FormBody.Builder()
+                .add("userid", UserInfoManager.getID(getActivity()))
+                .add("session", imei)
+                .build();
+        //创建一个Request
+        Request req = new Request.Builder()
+                .url(path)
+                .post(fb)
+                .build();
+        Call call = ohc.newCall(req);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String text = response.body().string();
+                Message msg = new Message();
+                msg.obj = text;
+                mHandler.sendMessage(msg);
+            }
+        });
+
+    }
+
+
+    public void stropration(Message msg) {              //分拣字符串
+        String[] strenter;
+        String msgobj;
+        msgobj = msg.obj.toString();
+        strenter = msgobj.split("\n");          //换行符分割不知道可不可行
+        num= strenter.length;
+        for (int i = 0; i < strenter.length; i++) {
+            strand[i] = strenter[i].split("&");     //按&划分
+        }
+
+
+    }
+
+
     @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
+    public void onListItemClick(ListView l, View v, int position, long id) {        //点击事件。待定
         super.onListItemClick(l, v, position, id);
 
         System.out.println(l.getChildAt(position));
-        HashMap<String, Object> view= (HashMap<String, Object>) l.getItemAtPosition(position);
-        System.out.println(view.get("title").toString()+"+++++++++title");
+        HashMap<String, Object> view = (HashMap<String, Object>) l.getItemAtPosition(position);
+        System.out.println(view.get("title").toString() + "+++++++++title");
 
-        Toast.makeText(getActivity(), TAG+l.getItemIdAtPosition(position), Toast.LENGTH_LONG).show();
+        Toast.makeText(getActivity(), TAG + l.getItemIdAtPosition(position), Toast.LENGTH_LONG).show();
         System.out.println(v);
 
         System.out.println(position);
     }
 
 
+    private List<? extends Map<String, ?>> getData(String[][] strand) {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 
+       for (int i = 0; i < num; i++) {
+           Map<String, Object> map = new HashMap<String, Object>();
+           map.put("title", strand[i][2]);
+           map.put("payment", strand[i][5]);
+           map.put("time", strand[i][3]);
 
-
-    private List<? extends Map<String, ?>> getData(String[] strs) {
-        List<Map<String ,Object>> list = new ArrayList<Map<String,Object>>();
+           list.add(map);
+        }
 
 /*        for (int i = 0; i < strs.length; i++) {
             Map<String, Object> map = new HashMap<String, Object>();
             map.put("title", strs[i]);
             list.add(map);
         }*/
-        Map<String, Object> map = new HashMap<String, Object>();
+
+/*        Map<String, Object> map = new HashMap<String, Object>();
         map.put("payment", "G1");
         map.put("time", "google 1");
         map.put("title", "tt");
@@ -90,7 +165,7 @@ public class TaskFragment extends ListFragment {
         map.put("payment", "G3");
         map.put("time", "google 3");
         map.put("title", "tt");
-        list.add(map);
+        list.add(map);*/
 
         return list;
     }
@@ -108,175 +183,5 @@ public class TaskFragment extends ListFragment {
         Log.i(TAG, "----------onAttach");
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-//failed
-
-
-    //private TextView thepayment,thetime,thetitle;
-    private ListView list;
-    private SimpleAdapter adapter;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_task_list, container, false);
-        list = (ListView) view.findViewById(android.R.id.list);
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1);
-        list.setAdapter(arrayAdapter);
-
-*/
-/*      thepayment = (TextView) view.findViewById(R.id.task_tv_payment);
-        thetime = (TextView) view.findViewById(R.id.task_tv_time);
-        thetitle = (TextView) view.findViewById(R.id.task_tv_task);*//*
-
-        //return inflater.inflate(R.layout.fragment_task_list, null);                //不知道这行和下面那行哪个是对的
-        return view;                                                                 //不知道这行和上面那行哪个是对的--好像这个是对的
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-
-
-    public class MyListView3 extends ListActivity {
-
-
-        // private List<String> data = new ArrayList<String>();
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            String[] listItem = { "a", "b", "c", "d", "e" };
-
-        */
-/*thepayment = (TextView)findViewById(R.id.task_tv_payment);
-        thetime = (TextView) findViewById(R.id.task_tv_time);
-        thetitle = (TextView)findViewById(R.id.task_tv_task);
-        setContentView(R.layout.fragment_task);*//*
-
-
-
-            adapter = new SimpleAdapter(getActivity(), getData(), R.layout.fragment_task,
-                    new String[]{"payment", "time", "title"},
-                    new int[]{R.id.task_tv_payment, R.id.task_tv_task, R.id.task_tv_time});
-            setListAdapter(adapter);
-        }
-
-*/
-/*        public void onCreate(Bundle savedInstanceState) {             //old version
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.fragment_task);
-
-
-            thepayment = (TextView)findViewById(R.id.task_tv_payment);
-            thetime = (TextView) findViewById(R.id.task_tv_time);
-            thetitle = (TextView)findViewById(R.id.task_tv_task);
-
-
-            setContentView(R.layout.fragment_task);
-            SimpleAdapter adapter = new SimpleAdapter(this, getData(), R.layout.fragment_task,
-                    new String[]{"payment", "time", "title"},
-                    new int[]{R.id.task_tv_payment, R.id.task_tv_task, R.id.task_tv_time});
-            setListAdapter(adapter);
-        }*//*
-
-
-        private List<Map<String, Object>> getData() {
-            List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-
-
-*/
-/*//*
-/每一块是一个list的内容
-            Map<String, Object> map = new HashMap<String, Object>();
-            map.put("payment", "G1");
-            map.put("time", "google 1");
-            map.put("title", "tt");
-            list.add(map);
-
-            map = new HashMap<String, Object>();
-            map.put("payment", "G2");
-            map.put("time", "google 2");
-            map.put("title", "tt");
-            list.add(map);
-
-            map = new HashMap<String, Object>();
-            map.put("payment", "G3");
-            map.put("time", "google 3");
-            map.put("title", "tt");
-            list.add(map);*//*
-
-
-            for (int i = 0; i < 5; i++) {
-                Map<String, Object> map = new HashMap<String, Object>();
-                map.put("payment", "G3");
-                map.put("time", "google 3");
-                map.put("title", "tt");
-                list.add(map);
-
-            }
-
-
-
-            return list;
-        }
-    }
-
-*/
 
 }
